82d0d5ebf49313febae317f58a8689959ea32050
vivekananda <vivekanandasahu.dev@gmail.com>
2025-08-06 11:44:56 +0530

    feat: add message for empty book list in BookList component

--- book-management-app/src/components/BookList.jsx
+++ book-management-app/src/components/BookList.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import BookItem from "./BookItem";
 
 const BookList = ({ books, onToggleRead, onDelete }) => {
+    if(books.length === 0) {
+        return <p>No Books to Display...</p>
+    }
   return (
     <>
       <ul>
