161343ff105508fc9aca670807dce9d74ec0743e
vivekananda <vivekanandasahu.dev@gmail.com>
2025-08-08 16:13:20 +0530

    refactor: implement caching for now playing movies

--- movie-watchlist-app/src/hooks/useNowPlayingMovies.js
+++ movie-watchlist-app/src/hooks/useNowPlayingMovies.js
@@ -1,17 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { fetchNowPlayingMovies } from "../utils/api/tmdb";
 
-export function useNowPlayingMovies(page=1) {
+export function useNowPlayingMovies(page = 1) {
   const [movies, setMovies] = useState([]);
   const [totalPages, setTotalPages] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const cacheRef = useRef({});
+
   useEffect(() => {
     async function loadMovies() {
+      if (cacheRef.current[page]) {
+        setMovies(cacheRef.current[page]);
+        setLoading(false);
+        return;
+      }
+
       const data = await fetchNowPlayingMovies(page);
       const { results, total_pages } = data;
 
       setMovies(results);
+      cacheRef.current[page] = results;
       setTotalPages(total_pages);
 
       setLoading(false);
