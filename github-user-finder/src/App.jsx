403b3806c0580cc7a61f309a579091aedce182fa
vivekananda <vivekanandasahu.dev@gmail.com>
2025-08-13 23:43:10 +0530

    feat: basic search input

--- github-user-finder/src/App.jsx
+++ github-user-finder/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
 import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = () => {
+    if (username.trim()) {
+      setSearchTerm(username.trim());
+    }
+  };
+
   return (
-    <>
+    <div className="app">
       <h1>GitHub user finder</h1>
-    </>
+
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Enter GitHub username..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={handleSearch}><IoSearch /></button>
+      </div>
+      {searchTerm && <p>Searching for: {searchTerm}</p>}
+    </div>
   );
 }
 
