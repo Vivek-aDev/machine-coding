ca38c7ec1911d4dcda5fcd54ebec20f36946a474
Vivekananda <vivekanandasahu.dev@gmail.com>
2025-08-14 15:50:46 +0530

    Feat/UI skeleton (#12)
    * add: initial setup
    
    * add: initial ui skeleton
    
    * add: style the Board and column component

---
+++ kanban-board/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Board from "./pages/Board";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Kanban Board</h1>
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
