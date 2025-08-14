0fbec7d7121ffc1955d941ed9a548884d3f5338a
Vivekananda <vivekanandasahu.dev@gmail.com>
2025-08-14 09:57:05 +0530

    feat: display data fetched from api (#11)

--- github-user-finder/src/App.jsx
+++ github-user-finder/src/App.jsx
@@ -5,10 +5,29 @@ import "./App.css";
 function App() {
   const [username, setUsername] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+  const [userData, setUserData] = useState(null);
 
-  const handleSearch = () => {
-    if (username.trim()) {
-      setSearchTerm(username.trim());
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSearch = async () => {
+    if (!username.trim()) return;
+
+    setSearchTerm(username.trim());
+    setLoading(true);
+    setError("");
+    setUserData(null);
+
+    try {
+      const res = await fetch(`https://api.github.com/users/${username}`);
+      if (!res.ok) throw new Error("User not found");
+
+      const data = await res.json();
+      setUserData(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -23,9 +42,30 @@ function App() {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <button onClick={handleSearch}><IoSearch /></button>
+        <button onClick={handleSearch}>
+          <IoSearch />
+        </button>
       </div>
-      {searchTerm && <p>Searching for: {searchTerm}</p>}
+
+      {/* loading/Error state handle */}
+      {loading && <p>Searching for: {searchTerm}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {userData && (
+        <div className="card">
+          <img src={userData.avatar_url} alt={userData.login} />
+          <h2>{userData.name || "No name"}</h2>
+          <p>{userData.bio || "No bio available"}</p>
+          <div className="stats">
+            <span>Repos: {userData.public_repos}</span>
+            <span>Followers: {userData.followers}</span>
+            <span>Followings: {userData.following}</span>
+          </div>
+          <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+            View Profile
+          </a>
+        </div>
+      )}
     </div>
   );
 }
